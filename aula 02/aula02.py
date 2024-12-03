20b737733d2cebe3fede95fc598c386c0df0297c
danielly <daniellysillva741@gmail.com>
2024-12-03 10:03:18 -0300

    Add files via upload

---
+++ aula 02/aula02.py
@@ -0,0 +1,11 @@
+print("bom dia a todos")
+
+nome_usuario ='carlos vinicius' #tipo - > str = strir
+idade_usuario = 21 # tipo -> int = integer
+altura_usuario =1,77 # tipo -> float = ponto flutuante
+maior_idade =True # tipo -> bool = boolaan
+
+print("seja bem-vindo", nome_usuario)
+print("sua idade é", idade_usuario, "anos")
+print("sua altura é", altura_usuario, "metros")
+print("você é maior de idade?", maior_idade)
